b5c4ad569bd50196c780f626b0c73568cb22690f
mohameddf <mohamedsamba95@gmail.com>
2019-10-17 17:15:28 +0200

    Workflow

--- src/workflow/Fraction.java
+++ src/workflow/Fraction.java
@@ -1,5 +1,25 @@
 package workflow;
 
 public class Fraction {
-
+	private int num,deno;
+	public Fraction(int num,int deno){
+		this.num=num;
+		this.deno=deno;}
+	
+	public Fraction(int num){
+		this.num=num;
+		this.deno=1;}
+	
+	public Fraction(){
+		this.num=0;
+		this.deno=1;}
+	public int getNum() {
+		return num;
+	}
+	public int getDeno() {
+		return deno;
+	}
+		public String toString() {
+		   return "numerateur et denominateur";
+		}
 }
