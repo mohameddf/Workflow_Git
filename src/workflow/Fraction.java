d11dc921d5a924f819791775da59f9cbb0253855
mamadou <mamadou.alhadj-adam@ens.uvsq.fr>
2019-10-17 15:17:52 +0200

    partie accesseurs

--- src/workflow/Fraction.java
+++ src/workflow/Fraction.java
@@ -2,4 +2,18 @@ package workflow;
 
 public class Fraction {
 
+
+
+//accesseurs
+public double getnum()
+     {
+          return num;
+     }
+public double getdenom()
+     {
+       return denom;
+     }
+
+
+
 }
